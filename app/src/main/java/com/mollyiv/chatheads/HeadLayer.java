d03d809ccfcf5640edd6d58a79caa2d848cdb1e0
Austin Lazarus <austin.lazarus@gmail.com>
2017-03-26 11:01:31 -0300

    commit - 26.03.17

--- app/src/main/java/com/mollyiv/chatheads/HeadLayer.java
+++ app/src/main/java/com/mollyiv/chatheads/HeadLayer.java
@@ -3,6 +3,7 @@ package com.mollyiv.chatheads;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.PixelFormat;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -21,11 +22,13 @@ import android.widget.Toast;
  * until another application does the same.
  *****************************************************************************/
 public class HeadLayer extends View {
-    private LinearLayout ll;
+    private LinearLayout ll, l2t, l3t, l4t, l5t, l6t;
+    private ImageView opacity_btn;
     private SeekBar sb;
     private Context mContext;
     private FrameLayout mFrameLayout;
     private WindowManager mWindowManager;
+    // =================================================================== //
     public HeadLayer(Context context) {
         super(context);
         mContext = context;
@@ -33,45 +36,56 @@ public class HeadLayer extends View {
         addTo_new_WindowManager();
         addToWindowManager();
     }
-
+    // =================================================================== //
     private void addTo_new_WindowManager() {
         final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
-                WindowManager.LayoutParams.MATCH_PARENT,
-                WindowManager.LayoutParams.MATCH_PARENT,
-                WindowManager.LayoutParams.TYPE_PHONE,
-                // WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
-                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
                 PixelFormat.TRANSLUCENT);
         mWindowManager = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
+        params.gravity = Gravity.END;
         ImageButton mButton = new ImageButton(mContext);
-        mButton.setImageResource(R.drawable.toggle);
+        mButton.setImageResource(R.drawable.on_off);
+        mButton.setBackgroundColor(Color.TRANSPARENT);
+        mButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, "Works", Toast.LENGTH_LONG).show();
+            }
+        });
         WindowManager wm = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
         wm.addView(mButton, params);
     }
-
-
+    // =================================================================== //
     private void addToWindowManager() {
-        final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+        WindowManager.LayoutParams params1 = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.MATCH_PARENT,
                 WindowManager.LayoutParams.MATCH_PARENT,
                 WindowManager.LayoutParams.TYPE_PHONE,
-               // WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
                 WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
                 PixelFormat.TRANSLUCENT);
-        mWindowManager = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
-        mWindowManager.addView(mFrameLayout, params);
 
+        WindowManager.LayoutParams params2 = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
+                PixelFormat.TRANSLUCENT);
+        mWindowManager = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
+        mWindowManager.addView(mFrameLayout, params2);
+        // =================================================================== //
         // Here is the place where you can inject whatever layout you want.
         LayoutInflater layoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         layoutInflater.inflate(R.layout.head, mFrameLayout);
 
-
         /**************************************************************************
          * Overlay Buttons
          **************************************************************************/
-        ImageView iv = (ImageView) mFrameLayout.findViewById(R.id.gadge);
+        opacity_btn = (ImageView) mFrameLayout.findViewById(R.id.gadge);
         ll = (LinearLayout) mFrameLayout.findViewById(R.id.l1);
-        iv.setOnClickListener(new OnClickListener() {
+        opacity_btn.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
                 ll.setSelected(!ll.isSelected());
@@ -82,38 +96,29 @@ public class HeadLayer extends View {
                 }
             }
         });
-//        ImageView above_head_toggle = (ImageView) mFrameLayout.findViewById(R.id.touch_toggle);
-//        above_head_toggle.setOnClickListener(new OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Toast.makeText(mContext, "Works!", Toast.LENGTH_LONG).show();
-//            }
-//        });
 
         /**************************************************************************
          * Overlay Touch
          **************************************************************************/
-        final LinearLayout l1t = (LinearLayout) mFrameLayout.findViewById(R.id.l1);
         final ImageView opacity_btn = (ImageView) mFrameLayout.findViewById(R.id.gadge);
         final Button toggle_btn = (Button) mFrameLayout.findViewById(R.id.toggle);
-        l1t.setOnTouchListener(new OnTouchListener() {
+        ll.setOnTouchListener(new OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
-                l1t.setSelected(!l1t.isSelected());
-                if (l1t.isSelected()) {
-                    l1t.setBackgroundColor(Color.TRANSPARENT);
+                ll.setSelected(!ll.isSelected());
+                if (ll.isSelected()) {
+                    ll.setBackgroundColor(Color.TRANSPARENT);
                     opacity_btn.setVisibility(GONE);
                     toggle_btn.setVisibility(GONE);
                 } else {
-                    l1t.setBackgroundColor(Color.BLACK);
+                    ll.setBackgroundColor(Color.BLACK);
                     opacity_btn.setVisibility(VISIBLE);
                     toggle_btn.setVisibility(VISIBLE);
                 }
-
                 return false;
             }
         });
-        final LinearLayout l2t = (LinearLayout) mFrameLayout.findViewById(R.id.l2);
+        l2t = (LinearLayout) mFrameLayout.findViewById(R.id.l2);
         l2t.setOnTouchListener(new OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -123,11 +128,10 @@ public class HeadLayer extends View {
                 } else {
                     l2t.setBackgroundColor(Color.BLACK);
                 }
-
                 return false;
             }
         });
-        final LinearLayout l3t = (LinearLayout) mFrameLayout.findViewById(R.id.l3);
+        l3t = (LinearLayout) mFrameLayout.findViewById(R.id.l3);
         l3t.setOnTouchListener(new OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -137,11 +141,10 @@ public class HeadLayer extends View {
                 } else {
                     l3t.setBackgroundColor(Color.BLACK);
                 }
-
                 return false;
             }
         });
-        final LinearLayout l4t = (LinearLayout) mFrameLayout.findViewById(R.id.l4);
+        l4t = (LinearLayout) mFrameLayout.findViewById(R.id.l4);
         l4t.setOnTouchListener(new OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -151,11 +154,10 @@ public class HeadLayer extends View {
                 } else {
                     l4t.setBackgroundColor(Color.BLACK);
                 }
-
                 return false;
             }
         });
-        final LinearLayout l5t = (LinearLayout) mFrameLayout.findViewById(R.id.l5);
+        l5t = (LinearLayout) mFrameLayout.findViewById(R.id.l5);
         l5t.setOnTouchListener(new OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -165,11 +167,10 @@ public class HeadLayer extends View {
                 } else {
                     l5t.setBackgroundColor(Color.BLACK);
                 }
-
                 return false;
             }
         });
-        final LinearLayout l6t = (LinearLayout) mFrameLayout.findViewById(R.id.l6);
+        l6t = (LinearLayout) mFrameLayout.findViewById(R.id.l6);
         l6t.setOnTouchListener(new OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -179,11 +180,9 @@ public class HeadLayer extends View {
                 } else {
                     l6t.setBackgroundColor(Color.BLACK);
                 }
-
                 return false;
             }
         });
-
 
         /**************************************************************************
          * Seekbar
@@ -192,49 +191,76 @@ public class HeadLayer extends View {
         sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                LinearLayout l = (LinearLayout) mFrameLayout.findViewById(R.id.l1);
-                LinearLayout l2 = (LinearLayout) mFrameLayout.findViewById(R.id.l2);
-                LinearLayout l3 = (LinearLayout) mFrameLayout.findViewById(R.id.l3);
                 if (i == 10) {
-                    l.setAlpha((float) 0.9);
-                    l2.setAlpha((float) 0.9);
-                    l3.setAlpha((float) 0.9);
+                    ll.setAlpha((float) 0.9);
+                    l2t.setAlpha((float) 0.9);
+                    l3t.setAlpha((float) 0.9);
+                    l4t.setAlpha((float) 0.9);
+                    l5t.setAlpha((float) 0.9);
+                    l6t.setAlpha((float) 0.9);
                 } else if (i == 20) {
-                    l.setAlpha((float) 0.8);
-                    l2.setAlpha((float) 0.8);
-                    l3.setAlpha((float) 0.8);
+                    ll.setAlpha((float) 0.8);
+                    l2t.setAlpha((float) 0.8);
+                    l3t.setAlpha((float) 0.8);
+                    l4t.setAlpha((float) 0.8);
+                    l5t.setAlpha((float) 0.8);
+                    l6t.setAlpha((float) 0.8);
                 } else if (i == 30) {
-                    l.setAlpha((float) 0.7);
-                    l2.setAlpha((float) 0.7);
-                    l3.setAlpha((float) 0.7);
+                    ll.setAlpha((float) 0.7);
+                    l2t.setAlpha((float) 0.7);
+                    l3t.setAlpha((float) 0.7);
+                    l4t.setAlpha((float) 0.7);
+                    l5t.setAlpha((float) 0.7);
+                    l6t.setAlpha((float) 0.7);
                 } else if (i == 40) {
-                    l.setAlpha((float) 0.6);
-                    l2.setAlpha((float) 0.6);
-                    l3.setAlpha((float) 0.6);
+                    ll.setAlpha((float) 0.6);
+                    l2t.setAlpha((float) 0.6);
+                    l3t.setAlpha((float) 0.6);
+                    l4t.setAlpha((float) 0.6);
+                    l5t.setAlpha((float) 0.6);
+                    l6t.setAlpha((float) 0.6);
                 } else if (i == 50) {
-                    l.setAlpha((float) 0.5);
-                    l2.setAlpha((float) 0.5);
-                    l3.setAlpha((float) 0.5);
+                    ll.setAlpha((float) 0.5);
+                    l2t.setAlpha((float) 0.5);
+                    l3t.setAlpha((float) 0.5);
+                    l4t.setAlpha((float) 0.5);
+                    l5t.setAlpha((float) 0.5);
+                    l6t.setAlpha((float) 0.5);
                 } else if (i == 60) {
-                    l.setAlpha((float) 0.4);
-                    l2.setAlpha((float) 0.4);
-                    l3.setAlpha((float) 0.4);
+                    ll.setAlpha((float) 0.4);
+                    l2t.setAlpha((float) 0.4);
+                    l3t.setAlpha((float) 0.4);
+                    l4t.setAlpha((float) 0.4);
+                    l5t.setAlpha((float) 0.4);
+                    l6t.setAlpha((float) 0.4);
                 } else if (i == 70) {
-                    l.setAlpha((float) 0.3);
-                    l2.setAlpha((float) 0.3);
-                    l3.setAlpha((float) 0.3);
+                    ll.setAlpha((float) 0.3);
+                    l2t.setAlpha((float) 0.3);
+                    l3t.setAlpha((float) 0.3);
+                    l4t.setAlpha((float) 0.3);
+                    l5t.setAlpha((float) 0.3);
+                    l6t.setAlpha((float) 0.3);
                 } else if (i == 80) {
-                    l.setAlpha((float) 0.2);
-                    l2.setAlpha((float) 0.2);
-                    l3.setAlpha((float) 0.2);
+                    ll.setAlpha((float) 0.2);
+                    l2t.setAlpha((float) 0.2);
+                    l3t.setAlpha((float) 0.2);
+                    l4t.setAlpha((float) 0.2);
+                    l5t.setAlpha((float) 0.2);
+                    l6t.setAlpha((float) 0.2);
                 } else if (i == 90) {
-                    l.setAlpha((float) 0.1);
-                    l2.setAlpha((float) 0.1);
-                    l3.setAlpha((float) 0.1);
+                    ll.setAlpha((float) 0.1);
+                    l2t.setAlpha((float) 0.1);
+                    l3t.setAlpha((float) 0.1);
+                    l4t.setAlpha((float) 0.1);
+                    l5t.setAlpha((float) 0.1);
+                    l6t.setAlpha((float) 0.1);
                 } else if (i == 100) {
-                    l.setAlpha(1);
-                    l2.setAlpha(1);
-                    l3.setAlpha(1);
+                    ll.setAlpha(0);
+                    l2t.setAlpha(0);
+                    l3t.setAlpha(0);
+                    l4t.setAlpha(0);
+                    l5t.setAlpha(0);
+                    l6t.setAlpha(0);
                 }
             }
 
